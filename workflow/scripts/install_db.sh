c0d5435651eb6f4d398ea1240512a1a7b54aedba
Aidan Foo <aidanfoo96@gmail.com>
2022-02-11 11:06:53 +0000

    Update install_db.sh

--- workflow/scripts/install_db.sh
+++ workflow/scripts/install_db.sh
@@ -6,7 +6,7 @@ conda activate install_db
 wget https://genome-idx.s3.amazonaws.com/kraken/k2_standard_20210517.tar.gz -P ../../resources/ 
 mkdir ../../resources/kraken_db
 mv ../../resources/k2_standard_20210517.tar.gz ../../resources/kraken_db
-tar xvzf --verbose ../../resources/kraken_db/k2_standard_20210517.tar.gz
+tar --verbose xvzf ../../resources/kraken_db/k2_standard_20210517.tar.gz
 
 # Install metaphlan_db
 metaphlan --install --bowtie2db ../../resources/
